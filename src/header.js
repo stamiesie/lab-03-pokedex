4126ebe36683f7cd1928cfaa558dd8097d683bc4
Stephen Tamiesie <stephen.tamiesie@gmail.com>
2021-02-11 17:09:32 -0800

    nav links working

--- src/header.js
+++ src/header.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
+import { NavLink } from 'react-router-dom';
 
 export default class Header extends Component {
     render() {
         return (
-            <div>
+            <header>
+                <NavLink exact activeClassName="selected" to="/">Home Page</NavLink>
+                <NavLink exact activeClassName="selected" to="/search">Search Page</NavLink>
+
                 <h1>Pokedex Sorter</h1>
-            </div>
+            </header>
+
         )
     }
 }
